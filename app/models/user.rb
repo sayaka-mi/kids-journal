2b6d57758b990135eb084186e2ce2524a59921b7
sayaka-mi <sayakakarai@gmail.com>
2025-06-09 16:31:49 +0900

    途中作業をコミット

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\S+@\S+\.\S+/ }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i , message: "は半角英数字をそれぞれ1文字以上含めて入力してください！"}, length: { minimum: 6}
 end
