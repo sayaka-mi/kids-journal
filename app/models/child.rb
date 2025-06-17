18e73ac9e0905f1bb84831a3c9f692b8070fdce9
sayaka-mi <sayakakarai@gmail.com>
2025-06-17 13:29:02 +0900

    erb/css調整

--- app/models/child.rb
+++ app/models/child.rb
@@ -2,7 +2,8 @@ class Child < ApplicationRecord
   belongs_to :user
 
   enum gender: { male: 0, female: 1 }
+  enum blood_type: { A: 'A', B: 'B', O: 'O', AB: 'AB', unknown: 'unknown' }
 
-  validates :name, :birthday, :gender, presence: true
+  validates :name, :birthday, :gender, :blood_type, presence: true
   validates :name, format: { with: /\S/, message:"を入力してください！"}
 end
