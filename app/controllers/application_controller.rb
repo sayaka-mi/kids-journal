db4801a007fec2a95bf944c051289d9c971f0e63
sayaka-mi <sayakakarai@gmail.com>
2025-06-21 14:33:15 +0900

    homescontroller削除ルーティング変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  include Devise::Controllers::Helpers
+  before_action :set_current_child
+  before_action :redirect_to_child_registration_if_none
 
   def after_sign_in_path_for(resource)
-    root_path
+    if resource.children.exists?
+      children_path
+    else
+      new_child_path
+    end
   end
 
   def after_sign_out_path_for(resource)
@@ -16,4 +21,27 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
+
+  def set_current_child
+    return unless user_signed_in?
+    @current_child = current_user.children.find_by(id: session[:child_id]) || current_user.children.first
+  end
+
+  def redirect_to_child_registration_if_none
+    return unless user_signed_in?
+    return if request.path == new_child_path
+    return if request.path.start_with?('/users/sign_out')
+    return if request.path.start_with?('/users')
+
+    if current_user.children.empty?
+      redirect_to new_child_path, alert: "お子さんの登録をしてね"
+      return
+    end
+  end
+
+  helper_method :current_child
+  def current_child
+    @current_child
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'homes#index'
+  root to: 'children#index'
 
   resources :children do
     resources :records, only: [:new, :create, :index, :edit, :update, :destroy]
   end
 
   resources :posts
+
+  post 'select_child', to: 'children#select_child', as: :select_child
 end
