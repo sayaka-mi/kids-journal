53265c75b44eafab2d0528e4e3a586cca89bf798
sayaka-mi <sayakakarai@gmail.com>
2025-06-24 15:52:06 +0900

    モデルの単体テスト・ja.yml修正・コントローラーテスト修正

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @tag = FactoryBot.build(:tag)
+  end
+
+  describe 'タグが有効な場合' do
+    it 'nameがあれば有効である' do
+      expect(@tag).to be_valid
+    end
+  end
+
+  describe 'タグが無効な場合' do
+    it 'nameが空では無効である' do
+      @tag.name = ''
+      @tag.valid?
+      expect(@tag.errors.full_messages).to include("タグ名 を入力してください")
+    end
+
+    it 'nameが重複している場合は無効である' do
+      FactoryBot.create(:tag, name: "テストタグ")
+      @tag.name = "テストタグ"
+      @tag.valid?
+      expect(@tag.errors.full_messages).to include("タグ名 は既に存在します")
+    end
+  end
 end
--- spec/requests/tags_spec.rb
+++ spec/requests/tags_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Tags", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/tags/index"
+      get "/tags"
       expect(response).to have_http_status(:success)
     end
   end
